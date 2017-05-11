ad1da200b530983d3be6d9ec77c995a3471e7593
386083520 <386083520@qq.com>
2017-05-11 20:15:43 +0800

    通过TypedArray获取属性值并绘制

--- app/src/main/java/com/example/administrator/autoattribute/MyView.java
+++ app/src/main/java/com/example/administrator/autoattribute/MyView.java
@@ -1,6 +1,10 @@
 package com.example.administrator.autoattribute;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,11 +13,27 @@ import android.view.View;
  */
 
 public class MyView extends View{
+    String name;
+    int age;
     public MyView(Context context, AttributeSet attrs) {
         super(context, attrs);
         for(int i=0;i<attrs.getAttributeCount();i++){
             attrs.getAttributeName(i);
             System.out.println(attrs.getAttributeName(i)+"---"+attrs.getAttributeValue(i));
         }
+
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.MyView);
+        name=ta.getString(R.styleable.MyView_name);
+        age=ta.getInt(R.styleable.MyView_age,0);
+
+
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        Paint paint=new Paint();
+        paint.setTextSize(40);
+        paint.setColor(Color.RED);
+        canvas.drawText(name+age+"",40,40,paint);
     }
 }
